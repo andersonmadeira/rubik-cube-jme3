60c2dd967702c77521f86d8a03ade9566050d39e
andersonmadeira <andersonmadeiracs@gmail.com>
2014-10-14 22:42:29 -0300

    Cube asset
    Now using a cube asset made in blender.

--- src/org/amad/rubik/Game.java
+++ src/org/amad/rubik/Game.java
@@ -1,6 +1,8 @@
 package org.amad.rubik;
 
 import com.jme3.app.SimpleApplication;
+import com.jme3.light.AmbientLight;
+import com.jme3.light.DirectionalLight;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Quaternion;
@@ -8,7 +10,9 @@ import com.jme3.math.Vector3f;
 import com.jme3.post.FilterPostProcessor;
 import com.jme3.post.filters.CartoonEdgeFilter;
 import com.jme3.renderer.RenderManager;
+import com.jme3.renderer.queue.RenderQueue;
 import com.jme3.scene.Geometry;
+import com.jme3.scene.Spatial;
 import com.jme3.scene.shape.Box;
 
 /**
@@ -17,26 +21,28 @@ import com.jme3.scene.shape.Box;
  */
 public class Game extends SimpleApplication {
     private static int LINS = 3, COLS = 3;
-    private Geometry boxes[][];
+    private Spatial cubes[][];
     @Override
     public void simpleInitApp() {
-        boxes = new Geometry[LINS][COLS];
-        ColorRGBA colors[] = {ColorRGBA.Cyan, ColorRGBA.Green, ColorRGBA.Red};
-        
+        cubes = new Spatial[LINS][COLS];
         
         // generate cubes
         for(int z = 0; z < 3; z++) {
             for (int i = 0; i < LINS; i++) {
                 for (int j = 0; j < COLS; j++) {
-                    boxes[i][j] = new Geometry("Box", new Box(0.5f, 0.5f, 0.5f));
-                    boxes[i][j].setLocalTranslation(new Vector3f(1.05f*j, 1.05f*i, 1.05f*z));
-                    Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-                    mat.setColor("Color", colors[z]);
-                    boxes[i][j].setMaterial(mat);
-                    rootNode.attachChild(boxes[i][j]);
+                    cubes[i][j] = assetManager.loadModel("Models/Cube/Cube.j3o");
+                    cubes[i][j].scale(0.5f);
+                    //cubes[i][j] = new Geometry("Box", new Box(0.5f, 0.5f, 0.5f));
+                    cubes[i][j].setLocalTranslation(new Vector3f(1.01f*j, 1.01f*i, 1.01f*z));
+                    rootNode.attachChild(cubes[i][j]);
                 }
             }
         }
+        
+        // adds a directional light so we can view the cubes
+        AmbientLight sun = new AmbientLight();
+        //sun.setDirection(new Vector3f(6.5f, 5.f, 7f));
+        rootNode.addLight(sun);
         
         viewPort.setBackgroundColor(ColorRGBA.White);
         
@@ -46,9 +52,9 @@ public class Game extends SimpleApplication {
         cam.lookAt(Vector3f.ZERO, cam.getUp());
         
         // enable cell shading
-        FilterPostProcessor fpp = new FilterPostProcessor(assetManager);
-        fpp.addFilter(new CartoonEdgeFilter());
-        viewPort.addProcessor(fpp);
+//        FilterPostProcessor fpp = new FilterPostProcessor(assetManager);
+//        fpp.addFilter(new CartoonEdgeFilter());
+//        viewPort.addProcessor(fpp);
     }
 
     @Override
